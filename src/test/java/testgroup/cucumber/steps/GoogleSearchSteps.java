e5339ade93cd197f5c1599c46cd6c5c9f89da3fa
kuldeepadhikari <kuldeepadh@gmail.com>
2021-03-09 10:00:07 -0500

    cucumber-java support
    cucumber-java support

--- src/test/java/testgroup/cucumber/steps/GoogleSearchSteps.java
+++ src/test/java/testgroup/cucumber/steps/GoogleSearchSteps.java
@@ -3,29 +3,34 @@ package testgroup.cucumber.steps;
 
 import browserPages.GoogleHomePage;
 import common.WebDriverFactory;
-import io.cucumber.java8.En;
 import org.openqa.selenium.WebDriver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 import static org.junit.Assert.assertEquals;
 
-public class GoogleSearchSteps implements En {
+public class GoogleSearchSteps {
     WebDriver webDriver;
     GoogleHomePage googleHomePage;
-    public GoogleSearchSteps() {
-        Given("^I am on the Google search page$", () -> {
-            webDriver=WebDriverFactory.createWebDriver();
-            googleHomePage =new GoogleHomePage(webDriver);
-        });
 
-        When("^I search for \"([^\"]*)\"$", (String searchString) -> {
-            googleHomePage.search(searchString);
-        });
+    @Given("^I am on the Google search page$")
+    public void ImOnGoogleSearchPage()
+    {
+        webDriver=WebDriverFactory.createWebDriver();
+        googleHomePage =new GoogleHomePage(webDriver);
+    };
 
-        Then("^the page title should start with \"([^\"]*)\"$", (String searchString) -> {
-            assertEquals(true, googleHomePage.getHeader().toLowerCase().startsWith(searchString));
-        });
+    @When("^I search for \"([^\"]*)\"$")
+    public void I_search_for(String searchString)
+    {
+        googleHomePage.search(searchString);
+    };
 
+    @Then("^the page title should start with \"([^\"]*)\"$")
+    public void thePageTitleShouldStartsWith(String searchString)
+    {
+        assertEquals(true, googleHomePage.getHeader().toLowerCase().startsWith(searchString));
     }
-
 
 }
